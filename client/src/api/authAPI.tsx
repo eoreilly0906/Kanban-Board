2876b118187f7c14db6156ab93a30bc9e575ca9a
Edward O <edwardoreilly0906@gmail.com>
2025-03-25 16:17:17 -0500

    login method created

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,16 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+    // TODO: make a POST request to the login route and return the response
+  const response = await fetch('/api/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userInfo),
+  });
+  return response.json();
 }
-
 
 
 export { login };
